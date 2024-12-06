2e8256c5b6ae3f0f2ecfbd48a456370050da76c3
Paulo Albuquerque <paulora2405@gmail.com>
2024-12-06 15:00:28 -0300

    Update template

--- 2024/rust/daily-template/src/part1.rs
+++ 2024/rust/daily-template/src/part1.rs
@@ -1,5 +1,6 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
+pub fn process(input: &str) -> miette::Result<String> {
+    dbg!(input);
     todo!("day 01 - part 1");
 }
 
@@ -9,7 +10,6 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
         let input = "";
         assert_eq!("", process(input)?);
         Ok(())
